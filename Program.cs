d719e0dd9cd7b7c6be5f071aacbf9eeca77b45a7
Alexandr <jwchaganav@gmail.com>
2022-11-25 17:19:46 +0400

    протестировал

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rnd = new Random();
+int m = rnd.Next(0,4);
+int n = rnd.Next(0,6);
+
+int resolt = Akkerman(m,n);
+Console.WriteLine($"m={m}, n={n} -> A(m,n) = {resolt}");
+
+int Akkerman(int m, int n)
+{
+    if(m == 0)
+        return n + 1;
+    else if(m > 0 && n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else if(m > 0 && n > 0)
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+    else
+        return 0;
+    
+}
